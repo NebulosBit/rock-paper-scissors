3c4c9eb38439305aeb1112bb8d4acfea3c25bde0
NebulosBit <127682659+NebulosBit@users.noreply.github.com>
2023-03-19 22:44:44 +0000

    Scripted function called getPlayerChoice that return "Rock", "Paper", and "Scissor"

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const rockPaperScissor = ["Rock", "Paper", "Scissor"]
 
+function getPlayerChoice(){
+  let playerChoice = prompt("Pick a choice: Rock, Paper, or Scissor");
+  playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase();
+  return playerChoice
+}
+
 function getComputerChoice(){
   const computerChoice = rockPaperScissor[(Math.floor(Math.random() * (rockPaperScissor.length)))];
-  return computerChoice
+  return computerChoice;
 }
 
 console.log(getComputerChoice());
+console.log(getPlayerChoice());
