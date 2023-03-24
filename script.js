b1c3e1ddff4cc41fe38c95ec80531fe7f2426eda
NebulosBit <127682659+NebulosBit@users.noreply.github.com>
2023-03-24 18:15:55 +0000

    Add getPlayerChoice function that allows the player to choose between Rock, Paper, or Scissors & Changed html id to value

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const rPSOption = ["Rock", "Paper", "Scissor"];
+const rpsButtons = document.querySelectorAll(".rps-button");
+
+function getPlayerChoice() {
+  for (let i = 0; i < rpsButtons.length; i++) {
+    rpsButtons[i].addEventListener("click", 
+    function() {
+      const playerChoice = this.getAttribute("value");
+    });
+  }
+}
 
 function getComputerChoice() {
   const randomNum = Math.floor(Math.random() * 3);
   let computerChoice = rPSOption[randomNum];
   return computerChoice;
 }
+
+getPlayerChoice()
